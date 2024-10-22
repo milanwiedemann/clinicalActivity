a7b37edf45e8bc8ab33926c6beae8f1d03564ebb
Arina Tamborska <143546177+atamborska@users.noreply.github.com>
2024-10-22 11:29:04 +0100

    Update list of urls, merge data extraction into one function

--- data-raw/icd10_code_usage.R
+++ data-raw/icd10_code_usage.R
@@ -7,28 +7,92 @@ library(here)
 url_start <- "https://files.digital.nhs.uk/"
 
 icd10_code_usage_urls <- list(
-  "2023to2024" = paste0(url_start, "A5/5B8474/hosp-epis-stat-admi-diag-2023-24-tab.xlsx"),
-  "2022to2023" = paste0(url_start, "7A/DB1B00/hosp-epis-stat-admi-diag-2022-23-tab_V2.xlsx"),
-  "2021to2022" = paste0(url_start, "0E/E70963/hosp-epis-stat-admi-diag-2021-22-tab.xlsx"),
-  "2020to2021" = paste0(url_start, "5B/AD892C/hosp-epis-stat-admi-diag-2020-21-tab.xlsx"),
-  "2019to2020" = paste0(url_start, "37/8D9781/hosp-epis-stat-admi-diag-2019-20-tab%20supp.xlsx"),
-  "2018to2019" = paste0(url_start, "1C/B2AD9B/hosp-epis-stat-admi-diag-2018-19-tab.xlsx"),
-  "2017to2018" = paste0(url_start, "B2/5CEC8D/hosp-epis-stat-admi-diag-2017-18-tab.xlsx"),
-  "2016to2017" = paste0(url_start, "publications/7/d/hosp-epis-stat-admi-diag-2016-17-tab.xlsx"),
-  "2015to2016" = paste0(url_start, "publicationimport/pub22xxx/pub22378/hosp-epis-stat-admi-diag-2015-16-tab.xlsx"),
-  "2014to2015" = paste0(url_start, "publicationimport/pub19xxx/pub19124/hosp-epis-stat-admi-diag-2014-15-tab.xlsx"),
-  "2013to2014" = paste0(url_start, "publicationimport/pub16xxx/pub16719/hosp-epis-stat-admi-diag-2013-14-tab.xlsx"),
-  "2012to2013" = paste0(url_start, "publicationimport/pub12xxx/pub12566/hosp-epis-stat-admi-diag-2012-13-tab.xlsx")
+  "fy22to23" = list(
+    file = paste0(url_start, "7A/DB1B00/hosp-epis-stat-admi-diag-2022-23-tab_V2.xlsx"),
+    sheet = 6,
+    skip_rows = 12,
+    usage_col = 8
+  ),
+  "fy23to24" = list(
+    file = paste0(url_start, "A5/5B8474/hosp-epis-stat-admi-diag-2023-24-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 12,
+    usage_col = 8
+  ),
+  "fy21to22" = list(
+    file = paste0(url_start, "0E/E70963/hosp-epis-stat-admi-diag-2021-22-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 12,
+    usage_col = 8
+  ),
+  "fy20to21" = list(
+    file = paste0(url_start, "5B/AD892C/hosp-epis-stat-admi-diag-2020-21-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 12,
+    usage_col = 8
+  ),
+  "fy19to20" = list(
+    file = paste0(url_start, "37/8D9781/hosp-epis-stat-admi-diag-2019-20-tab%20supp.xlsx"),
+    sheet = 6,
+    skip_rows = 11,
+    usage_col = 8
+  ),
+  "fy18to19" = list(
+    file = paste0(url_start, "1C/B2AD9B/hosp-epis-stat-admi-diag-2018-19-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 11,
+    usage_col = 8
+  ),
+  "fy17to18" = list(
+    file = paste0(url_start, "B2/5CEC8D/hosp-epis-stat-admi-diag-2017-18-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 11,
+    usage_col = 8
+  ),
+  "fy16to17" = list(
+    file = paste0(url_start, "publications/7/d/hosp-epis-stat-admi-diag-2016-17-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 11,
+    usage_col = 8
+  ),
+  "fy15to16" = list(
+    file = paste0(url_start, "publicationimport/pub22xxx/pub22378/hosp-epis-stat-admi-diag-2015-16-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 12,
+    usage_col = 8
+  ),
+  "fy14to15" = list(
+    file = paste0(url_start, "publicationimport/pub19xxx/pub19124/hosp-epis-stat-admi-diag-2014-15-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 12,
+    usage_col = 8
+  ),
+  "fy13to14" = list(
+    file = paste0(url_start, "publicationimport/pub16xxx/pub16719/hosp-epis-stat-admi-diag-2013-14-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 18,
+    usage_col = 3
+  ),
+  "fy12to13" = list(
+    file = paste0(url_start, "publicationimport/pub12xxx/pub12566/hosp-epis-stat-admi-diag-2012-13-tab.xlsx"),
+    sheet = 6,
+    skip_rows = 19,
+    usage_col = 3
+  )
 )
 
-read_icd10_usage_xlsx_from_url <- function(url, sheet = 6, skip = 11, ...) {
+read_icd10_usage_from_url <- function(url_list, sheet = 6, skip = 11, icd10_code_col_num = 1, description_col_num = 2, usage_col_num = 8, ...) {
+  url <- sapply(url_list, function(x) x$file)
+  sheet <- ifelse(is.na(url_list$sheet), sheet, url_list$sheet)
+  skip <- ifelse(is.na(url_list$skip_rows), skip, url_list$skip_rows)
+  usage_col_num <- ifelse(is.na(url_list$usage_col), usage_col_num, url_list$usage_col)
   tmp_xlsx <- tempfile()
   download.file(
     url,
     destfile = "tmp_xlsx",
     mode = "wb"
   )
-  readxl::read_xlsx(
+  data <- readxl::read_xlsx(
     "tmp_xlsx",
     col_names = FALSE,
     .name_repair = janitor::make_clean_names,
@@ -37,17 +101,11 @@ read_icd10_usage_xlsx_from_url <- function(url, sheet = 6, skip = 11, ...) {
     ...
   )
   unlink(tmp_xlsx)
-}
-
-select_all_diag_counts <- function(data) {
-  #need to define year here
-  usage_col <-  ifelse(year >=2015, 3, 8)
-
   dplyr::select(
     data,
-    icd10_code = 1,
-    description = 2,
-    usage = usage_col
+    icd10_code = icd10_code_col_num,
+    description = description_col_num,
+    usage = usage_col_num
   ) |>
     # separate(icd10_code, c("icd10_category", "icd10_subclassification"), "\\.", remove = FALSE) |>
     dplyr::mutate(
@@ -56,8 +114,7 @@ select_all_diag_counts <- function(data) {
 }
 
 icd10_usage <- icd10_code_usage_urls |>
-  map(read_icd10_usage_xlsx_from_url) |>
-  map(select_all_diag_counts) |>
+  map(read_icd10_usage_from_url) |>
   bind_rows(.id = "nhs_fy") |>
   separate(nhs_fy, c("start_date", "end_date"), "to") |>
   mutate(
@@ -70,6 +127,3 @@ usethis::use_data(
   compress = "bzip2",
   overwrite = TRUE
 )
-
-# icd10_usage |>
-#   filter(str_detect(icd10_code, "A00"))
